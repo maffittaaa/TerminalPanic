91ccdf99511151fd0b2e99fdd183ed7e1fd0fec3
artugatuga <148588412+artugatuga@users.noreply.github.com>
2025-03-19 01:30:13 +0000

    head bob looking ok, having some preoblems when beating with the walls, camera starts shaking

--- Assets/Scripts/Camera/Effects/HeadBob.cs
+++ Assets/Scripts/Camera/Effects/HeadBob.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class HeadBob : MonoBehaviour
 {
     [SerializeField] private float headDetor;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
     [SerializeField] private float speed;
     [SerializeField] private GameObject viewPoint;
+    [SerializeField] private PlayerMovement playerMovementScript;
+    [SerializeField] private float newPosition;
     private float initialYPosition;
     private float currentYPosition;
-
 
     private void Start()
     {
@@ -19,22 +22,36 @@ public class HeadBob : MonoBehaviour
 
     private void Update()
     {
-        DetorEffect();
+        if (playerMovementScript != null && playerMovementScript.CurrentType() != BehaviorType.Idleing)
+        {
+            if(playerMovementScript.CurrentType() == BehaviorType.Walking)
+            {
+                speed = walkSpeed;
+            }
+
+            if (playerMovementScript.CurrentType() == BehaviorType.Runing)
+            {
+                speed = runSpeed;
+            }
+
+            DetorEffect();
+        }
+        else
+        {
+            currentYPosition = Mathf.Lerp(currentYPosition, initialYPosition, Time.deltaTime * 2f);
+            viewPoint.transform.position = new Vector3(viewPoint.transform.position.x, currentYPosition, viewPoint.transform.position.z);
+        }
     }
 
     private void DetorEffect()
     {
-        if (currentYPosition > initialYPosition + headDetor) 
-        {
-            speed *= -1;
-        }
-        else if(currentYPosition < initialYPosition - headDetor)
-        {
-            speed *= -1;
-        }
+        currentYPosition = initialYPosition + headDetor * Mathf.Sin(Time.time * speed);
 
-        currentYPosition = currentYPosition + speed;
+        viewPoint.transform.position = new Vector3(viewPoint.transform.position.x, currentYPosition, viewPoint.transform.position.z);
+    }
 
-        viewPoint.transform.position = new Vector3(viewPoint.transform.position.x, currentYPosition);
+    private float MakePositive(float num)
+    {
+        return Mathf.Sqrt(Mathf.Pow(num, 2));
     }
 }
--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -2,24 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BehaviorType { Idleing , Walking, Runing }
+
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rb;
     [SerializeField] private int speed;
+    [SerializeField] private int sprintMultiplier;
     [SerializeField] private int jumpForce;
+    private int initialSpeed;
 
     [SerializeField] private float horizontalAxis;
     [SerializeField] private float verticalAxis;
 
+    private BehaviorType behaviorType;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        initialSpeed = speed;
+        behaviorType = BehaviorType.Idleing;
     }
 
     private void Update()
     {
         //Movement
         Movement();
+        Sprint();
 
         //Jump
         Jump();
@@ -30,13 +39,36 @@ public class PlayerMovement : MonoBehaviour
         horizontalAxis = Input.GetAxis("Horizontal");
         verticalAxis = Input.GetAxis("Vertical");
 
-
-        Vector3 moveDirection = Vector3.zero;
+        Vector3 moveDirection;
 
         moveDirection = new Vector3(horizontalAxis * speed, rb.velocity.y, verticalAxis * speed);
         moveDirection = transform.TransformDirection(moveDirection);
 
         rb.velocity = moveDirection;
+    }    
+    
+    private void Sprint()
+    {
+        if (horizontalAxis != 0 || verticalAxis != 0)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                behaviorType = BehaviorType.Runing;
+                if (initialSpeed * sprintMultiplier > speed)
+                {
+                    speed *= sprintMultiplier;
+                }
+            }
+            else
+            {
+                behaviorType = BehaviorType.Walking;
+                speed = initialSpeed;
+            }
+        }
+        else
+        {
+            behaviorType = BehaviorType.Idleing;
+        }
     }
 
     private void Jump()
@@ -45,5 +77,10 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
         }
+    }
+
+    public BehaviorType CurrentType()
+    {
+        return behaviorType;
     }
 }
