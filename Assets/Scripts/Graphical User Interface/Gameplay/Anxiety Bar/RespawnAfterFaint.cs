466fa2e12a295680dfca31e5d131cec18c3a1304
Maffittaa <mafaldamartins2001@gmail.com>
2025-03-27 17:37:53 +0000

    twiked a few values

--- Assets/Scripts/Graphical User Interface/Gameplay/Anxiety Bar/RespawnAfterFaint.cs
+++ Assets/Scripts/Graphical User Interface/Gameplay/Anxiety Bar/RespawnAfterFaint.cs
@@ -13,7 +13,7 @@ public class RespawnAfterFaint : MonoBehaviour
             this.transform.position = new Vector3(27.53f,3.79f,-301.08f);
             anxietyBar.currentAnxiety = 0f;
             anxietyBar.state.fillAmount = 0f;
-            anxietyBar.dOF.focusDistance.value = 3.3f;
+            anxietyBar.dOF.focusDistance.value = 3.8f;
             anxietyBar.cA.intensity.value = 0.3f;
         }
     }
