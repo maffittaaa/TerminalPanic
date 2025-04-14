0963ab98318009ca0aca9792f8f4eb7fbfd17f42
Maffittaa <mafaldamartins2001@gmail.com>
2025-04-14 11:56:53 +0100

    thief instantiates with no bugs

--- Assets/Scripts/The Voice/Behavior.cs
+++ Assets/Scripts/The Voice/Behavior.cs
@@ -24,17 +24,17 @@ public class Behavior : MonoBehaviour
         clothesAndAccessoriesNumber = Random.Range(3, Enum.GetValues(typeof(EClothesAndAccessoriesTypes)).Length - 1);
         int i = 0;
         int k = 0;
-        bool equalType = false;
+        
         while (i < clothesAndAccessoriesNumber)
         {
+            bool equalType = false;
             Debug.Log("i: " + i);
             Debug.Log("clothes: " + clothesAndAccessoriesNumber);
-            if (k >= 100)
+            if (k >= 1000)
             {
                 Debug.Log("something went wrong");
                 break;
             }
-
             
             EClothesAndAccessoriesTypes clothesType = (EClothesAndAccessoriesTypes)Random.Range(1, Enum.GetValues(typeof(EClothesAndAccessoriesTypes)).Length - 1);
             for (int j = 0; j < hintsForThief.clothesAndAccessoriesTypes.Count; j++)
@@ -43,7 +43,6 @@ public class Behavior : MonoBehaviour
                 Debug.Log("types of clothes number: " + hintsForThief.clothesAndAccessoriesTypes.Count);
                 if (clothesType == hintsForThief.clothesAndAccessoriesTypes[j])
                 {
-                    Debug.Log("OH NO");
                     equalType = true;
                     break;
                 }
@@ -52,24 +51,24 @@ public class Behavior : MonoBehaviour
             if (!equalType)
             {
                 EColorTypes colorsType = (EColorTypes)Random.Range(1, Enum.GetValues(typeof(EColorTypes)).Length - 1);
-                hintsForThief.colorTypes.Add(colorsType);
                 hintsForThief.clothesAndAccessoriesTypes.Add(clothesType);
+                hintsForThief.colorTypes.Add(colorsType);
                 i++;
             }
             k++;
         }
     }
-
-    public void IdentifyingTheCorrectClues(int clothesAndAccessories, int color)
-    {
-        EClothesAndAccessoriesTypes clueType1 = hintsForThief.clothesAndAccessoriesTypes[clothesAndAccessories];
-        
-       if (!howManyHasPlayerSeen.ContainsKey(clueType1))
-         howManyHasPlayerSeen[clueType1] = 0;
-
-        howManyHasPlayerSeen[clueType1]++;
-            
-        if (howManyHasPlayerSeen[clueType1] == peopleNeededToNextClue.ConvertTo<EColorTypes>()) //this is wrong!!!!!!
-           clue.TextForClue();
-    }
+    
+    // public void IdentifyingTheCorrectClues(int clothesAndAccessories, int color)
+    // {
+    //     EClothesAndAccessoriesTypes clueType1 = hintsForThief.clothesAndAccessoriesTypes[clothesAndAccessories];
+    //     
+    //    if (!howManyHasPlayerSeen.ContainsKey(clueType1))
+    //      howManyHasPlayerSeen[clueType1] = 0;
+    //
+    //     howManyHasPlayerSeen[clueType1]++;
+    //         
+    //     if (howManyHasPlayerSeen[clueType1] == peopleNeededToNextClue.ConvertTo<EColorTypes>()) //this is wrong!!!!!!
+    //        clue.TextForClue();
+    // }
 }
